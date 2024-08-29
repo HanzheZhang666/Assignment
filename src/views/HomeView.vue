a1b8fca8b9e00a18f5765dc1630764c5ca8c4352
hzha0304 <hzha0304@student.monash.edu>
2024-08-30 02:37:04 +1000

    push 1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to Elderly Support Application</h1>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
